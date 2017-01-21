f4e8fc796fe03d6582ef862a09803dcfe5cb8598
souseisekichan <souseiseki_Chan@mail.ru>
2017-01-21 10:00:22 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telepot import *
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 from telepot.namedtuple import InlineQueryResultArticle, InputTextMessageContent
-
+import random
 
 #bot = telebot.TeleBot("316846706:AAF3ipEWyE5l0ASJ5MN-7X6zEchuhhP2uYg")
 
@@ -11,25 +11,33 @@ db_api = 'LDX1tde9wZSyJfsh4m80tciZ4gegCoc12EtC5oS7Ibo'
 #-----------------------------------------------------------
 
 
-
-
 def command_handle(s):
     d = {}
-    with open("dict.json") as f:
-        for line in f:
-            (key, val) = line.split()
-            d[key] = val
-        ans = d.get(s)
-        if ans == None:
-            ans = 'Try again!'
+    with open('dict.json') as data_file:
+    	d = json.load(data_file)
+    	ans = d.get(s)
+    if ans == None:
+        pass
     return ans
+
 
 def on_message(msg): #answering message! 
     content_type, chat_type, chat_id = glance(msg)
-    if msg['text'].lower().find('куко') != -1 or msg['text'].lower().find('кууко') != -1:
-        s_str = msg['text'].split()
-        ans = command_handle(s_str[2].lower())
-        bot.sendMessage(chat_id, ans)
+
+    '''bot.sendMessage('278278709', msg['from'])
+                bot.sendMessage('278278709', msg['text'])'''
+    #==============================================
+    if msg['text'].lower().find('roll') != -1 :
+    	try:
+    		s = int(msg['text'].lower().split()[1])
+    		bot.sendMessage(chat_id, random.randint(0, s))
+    	except:
+    		bot.sendMessage(chat_id, 'Сообщение должно содержать только roll и одну цифру, бвака!')
+    #================================================
+    
+    elif command_handle(msg['text'].lower()) != None :
+    	bot.sendMessage(chat_id, command_handle(msg['text'].lower()), parse_mode = 'Markdown', reply_to_message_id = msg)
+
 
 
 
@@ -41,4 +49,5 @@ bot.message_loop({'chat': on_message,
                   #'chosen_inline_result': on_chosen_inline_result
                   },
                  run_forever='Listening ...')
+
 
